78b83cdf30aeda39f971eb849d147af02ceae1b2
Hashim Karim <hashimkarim168@gmail.com>
2025-10-29 18:03:16 +0100

    Update to mount any canable version

--- dronecan.sh
+++ dronecan.sh
@@ -2,7 +2,10 @@
 
 # Find the ACM port for CANable by matching the product string
 CANABLE_PORT=$(for dev in /dev/ttyACM*; do
-    udevadm info -a -n "$dev" | grep -q 'ATTRS{product}=="CANable2 b158aa7 github.com/normaldotcom/canable2.git"' && echo "$dev"
+    if udevadm info -a -n "$dev" | grep -qi 'ATTRS{product}.*canable'; then
+        echo "$dev"
+        break
+    fi
 done | head -n1)
 
 if [ -z "$CANABLE_PORT" ]; then
@@ -15,6 +18,3 @@ sudo ifconfig can0 up
 
 # Start DroneCAN GUI tool in background
 dronecan_gui_tool &
-
-# Start Feetech Instruction GUI
-# python3 tools/feetech_instruction_gui.py --iface can0 --node-id 100
